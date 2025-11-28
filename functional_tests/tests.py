aee34967bbaab829b3df396a4cfa2a48e73968b1
Alex Gregory <axander.gregory@gmail.com>
2025-11-28 17:37:14 +0000

    rename functional tests and use LiveServerTestCase helper

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,12 +1,13 @@
 import time
 import unittest
 
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self) -> None:
         self.browser = webdriver.Firefox()
 
@@ -21,7 +22,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_todo_list(self):
         # Edith has heard about a cool new online to-do app.
         # She goes to check out its homepage
-        self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn("To-Do", self.browser.title)
@@ -54,8 +55,3 @@ class NewVisitorTest(unittest.TestCase):
         self.check_for_row_in_list_table("1: Buy peacock feathers")
 
         # Satisfied, she goes back to sleep
-
-
-if __name__ == "__main__":
-    unittest.main()
-
