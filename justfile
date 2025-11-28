659f3c149451dd6a3d9a96210e9b65f678b05f56
Alex Gregory <axander.gregory@gmail.com>
2025-11-28 08:26:31 +0000

    refactor homepage view to use template

--- justfile
+++ justfile
@@ -1,6 +1,9 @@
 default:
     just --list
 
+serve:
+    uv run manage.py runserver
+
 # Run tests
 test *args:
     uv run manage.py test
--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,10 @@ from django.test import TestCase
 class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         response = self.client.get("/")
-        self.assertContains(response, "<title>To-Do lists</title>")
-        self.assertContains(response, "<html>")
-        self.assertContains(response, "</html>")
+        # rather than targeting specific html, we can target correct template used
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_homepage_content(self):
+        response = self.client.get("/")
+        # smoke test: check system is doing one very simple thing correctly
+        self.assertContains(response, "To-Do")
